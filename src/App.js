2a44d22a05aa5160543dd729a86a217d5156f8f1
Tolani Daniel <mumytosin2016@gmail.com>
2022-10-28 04:39:32 +0100

    updated: completed media Qs for mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Profile from "./components/Profile";
 import Links from "./components/Links";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import Socials from "./components/Socials";
 
-import '../src/styles/App.css'
+import "../src/styles/App.css";
 
 function App() {
   return (
@@ -13,15 +13,39 @@ function App() {
         img="https://i.imgur.com/OW6krjH.jpeg"
       />
 
-      <Links linkAddress='https://twitter.com/daniel_tolani_' id="btn__twit" linkName="Twitter Link" />
-      <Links linkAddress='https://training.zuri.team' id="btn__zuri" linkName="Zuri Team" />
-      <Links linkAddress='http://books.zuri.team'  id="books" linkName="Zuri Books" />
-      <Links linkAddress='https://books.zuri.team/python-for-beginners?ref_id=<TDo8>' id="book__python" linkName="Python Books" />
-      <Links linkAddress='https://background.zuri.team' id="pitch" linkName="Background Check for Coders" />
-      <Links linkAddress='http://books.zuri.team/design-rules' id="book__design" linkName="Design Books" />
+      <Links
+        linkAddress="https://twitter.com/daniel_tolani_"
+        id="btn__twit"
+        linkName="Twitter Link"
+      />
+      <Links
+        linkAddress="https://training.zuri.team"
+        id="btn__zuri"
+        linkName="Zuri Team"
+      />
+      <Links
+        linkAddress="http://books.zuri.team"
+        id="books"
+        linkName="Zuri Books"
+      />
+      <Links
+        linkAddress="https://books.zuri.team/python-for-beginners?ref_id=<TDo8>"
+        id="book__python"
+        linkName="Python Books"
+      />
+      <Links
+        linkAddress="https://background.zuri.team"
+        id="pitch"
+        linkName="Background Check for Coders"
+      />
+      <Links
+        linkAddress="http://books.zuri.team/design-rules"
+        id="book__design"
+        linkName="Design Books"
+      />
 
-    <Socials />
-    <Footer footerText="HNG Internship 9 Frontend Task" />
+      <Socials />
+      <Footer footerText="HNG Internship 9 Frontend Task" />
     </div>
   );
 }
